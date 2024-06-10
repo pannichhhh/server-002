0abb2a3715a92027203f8257cd40f7738a53a329
pannichhhh <panichakam.k@gmail.com>
2024-06-10 09:17:26 +0700

    Swagger PATH

---
+++ swagger.js
@@ -0,0 +1,25 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Employee API',
+            version: '1.0.0',
+            description: 'API ตัวอย่างสำหรับ Employee Profile ',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Development server',
+            },
+        ],
+    },
+    //apis: ['./server002.js'],
+    apis: ['./controllers/controlemp.js'],  
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = { swaggerSpec, swaggerUi };
